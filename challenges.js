47f7d41cc1d9a4f8ea0c45ac902318ebce83ba84
krishan chikara <krishan.chikara@gmail.com>
2022-02-08 12:56:17 +0530

    Array Methods

--- challenges.js
+++ challenges.js
@@ -751,7 +751,7 @@ GOOD LUCK �
  // This generates [0, 0, 0, 0]. More in the next section!
  answers: new Array(4).fill(0),
  };
-*/
+
 
 // The Complete JavaScript Course 22
 // Coding Challenge #2
@@ -772,32 +772,53 @@ GOOD LUCK �
 })();
 
 //  GOOD LUCK �
+
+// The Complete JavaScript Course 23
+// Working With Arrays
+// Coding Challenge #1
+// Julia and Kate are doing a study on dogs. So each of them asked 5 dog owners about their dog's age, and stored the data into an array (one array for each). For now, they are just interested in knowing whether a dog is an adult or a puppy. A dog is an adult if it is at least 3 years old, and it's a puppy if it's less than 3 years old.
+
+// Your tasks:
+// Create a function 'checkDogs', which accepts 2 arrays of dog's ages ('dogsJulia' and 'dogsKate'), and does the following things:
+
+const dogsJulia = [3, 5, 2, 12, 7];
+const dogsKate = [4, 1, 15, 8, 3];
+
+const checkDogs = function (dogsJulia, dogsKate) {
+  const dogsJuliaCorrected = dogsJulia.slice(1, 3);
+  const dogs = dogsJuliaCorrected.concat(dogsKate);
+  dogs.forEach(function (dogAge, i) {
+    if (dogAge >= 3) {
+      console.log(
+        `Dog number ${i + 1} is an adult, and is ${dogAge} years old`
+      );
+    } else {
+      console.log(`Dog number ${i + 1} is still a puppy 🐶`);
+    }
+  });
+};
+checkDogs(dogsJulia, dogsKate);
+
+// 1. Julia found out that the owners of the first and the last two dogs actually have cats, not dogs! So create a shallow copy of Julia's array, and remove the cat ages from that copied array (because it's a bad practice to mutate function parameters)
+
+// const newDogJuliaList = dogsJulia.slice(1, 3);
+// console.log(newDogJuliaList, dogsJulia);
+
+// 2. Create an array with both Julia's (corrected) and Kate's data
+
+// const dogList = newDogJuliaList.concat(dogsKate);
+// console.log(dogList);
+
+// 3. For each remaining dog, log to the console whether it's an adult ("Dog number 1 is an adult, and is 5 years old") or a puppy ("Dog number 2 is still a puppy � ")
+// 4. Run the function for both test datasets
+// Test data:
+//§ Data 1: Julia's data [3, 5, 2, 12, 7], Kate's data [4, 1, 15, 8, 3]
+//§ Data 2: Julia's data [9, 16, 6, 8, 3], Kate's data [10, 5, 6, 1, 4]
+
+//Hints: Use tools from all lectures in this section so far �
+*/
+//GOOD LUCK �
 /*
-The Complete JavaScript Course 23
-Working With Arrays
-Coding Challenge #1
-Julia and Kate are doing a study on dogs. So each of them asked 5 dog owners 
-about their dog's age, and stored the data into an array (one array for each). For 
-now, they are just interested in knowing whether a dog is an adult or a puppy.
-A dog is an adult if it is at least 3 years old, and it's a puppy if it's less than 3 years 
-old.
-Your tasks:
-Create a function 'checkDogs', which accepts 2 arrays of dog's ages 
-('dogsJulia' and 'dogsKate'), and does the following things:
-1. Julia found out that the owners of the first and the last two dogs actually have 
-cats, not dogs! So create a shallow copy of Julia's array, and remove the cat 
-ages from that copied array (because it's a bad practice to mutate function 
-parameters)
-2. Create an array with both Julia's (corrected) and Kate's data
-3. For each remaining dog, log to the console whether it's an adult ("Dog number 1 
-is an adult, and is 5 years old") or a puppy ("Dog number 2 is still a puppy 
-� ")
-4. Run the function for both test datasets
-Test data:
-§ Data 1: Julia's data [3, 5, 2, 12, 7], Kate's data [4, 1, 15, 8, 3]
-§ Data 2: Julia's data [9, 16, 6, 8, 3], Kate's data [10, 5, 6, 1, 4]
-Hints: Use tools from all lectures in this section so far �
-GOOD LUCK �
 The Complete JavaScript Course 24
 Coding Challenge #2
 Let's go back to Julia and Kate's study about dogs. This time, they want to convert 
@@ -817,6 +838,7 @@ Test data:
 § Data 1: [5, 2, 4, 1, 15, 8, 3]
 § Data 2: [16, 6, 10, 5, 6, 1, 4]
 GOOD LUCK �
+
 Coding Challenge #3
 Rewrite the 'calcAverageHumanAge' function from Challenge #2, but this time 
 as an arrow function, and using chaining!
